2cdfd747ac3900b4d18c3290e898645321974225
KhairunFatihah <104422165+KhairunFatihah@users.noreply.github.com>
2022-04-26 16:27:32 +0800

    Create guess number-1.py

---
+++ guess number-1.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import random
+
+
+def main():
+    """Start a number guessing game between 1 - 100."""
+    print("guess the number!")
+
+    X = random.gammavariate(1, 100)
+    # x = random.betavariate(1, 2)
+    print(X)
+    guess = None
+
+    while X != guess:
+
+        guess = int(input("Pick a number between 1 to 100: "))
+
+        if X == guess:
+            print("You genius!")
+            break
+        else:
+            print("lol. lmao")
+           
+main()
